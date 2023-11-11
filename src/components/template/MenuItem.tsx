679f2c91ffef085ab67df6acebab4c2da62a0b4a
EricksonDutra <ericksond10@gmail.com>
2023-11-11 16:27:32 -0400

    components e dark theme

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,31 +1,41 @@
 import Link from "next/link";
 
-interface MenuItemProps{
-    url: string
-    texto: string
-    icone: any
+interface MenuItemProps {
+  texto: string;
+  icone: any;
+  url?: string;
+  className?: string;
+  onClick?: (ev: any) => void;
 }
 
 export default function MenuItem(props: MenuItemProps) {
+  function renderizarLink() {
     return (
-      <li
+      <a
+        href={props.url}
         className={`
-        hover:bg-gray-100
-    `}
+      flex flex-col justify-center items-center
+      h-20 w-20   dark:text-gray-200
+      ${props.className}
+      `}
       >
-        <Link
-          href={props.url}
-          className={`
-                flex flex-col justify-center items-center
-                h-20 w-20
-            `}
-        >
-            {props.icone}
-            <span className="text-xs font-light text-gray-600">
-              {props.texto}
-            </span>
-        </Link>
-      </li>
+        {props.icone}
+        <span className="text-xs font-light ">{props.texto}</span>
+      </a>
     );
-
+  }
+  
+  return (
+    <li onClick={props.onClick} 
+    className={`
+    hover:bg-gray-100 
+    dark:hover:bg-gray-700 
+    cursor-pointer  dark:text-gray-200`}>
+      {props.url ? (
+        renderizarLink()
+      ) : (
+        renderizarLink()
+      )}
+    </li>
+  );
 }
--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,15 +1,48 @@
-import { IconeAjustes, IconeCasa, IconeSino } from "../icons/Icones";
+import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from "../icons/Icones";
 import MenuItem from "./MenuItem";
+import Image from "next/image";
 
 export default function MenuLateral() {
   return (
-    <aside>
-      <ul>
+    <aside className={
+      `flex flex-col
+      bg-gray-200 dark:text-gray-700
+       dark:bg-gray-900
+       `
+       }>
+      <div
+        className={`
+        flex flex-col items-center justify-center
+        bg-gradient-to-r from-indigo-500 to-purple-800
+        w-20 h-20
+      `}
+      >
+        <Image
+          className="rounded-sm"
+          src={"/pib.png"}
+          alt="logo"
+          width={70}
+          height={70}
+        />
+        {/* <img src="/pib.png" alt="logo" /> */}
+      </div>
+      <ul className="flex-grow " >
         <MenuItem url="/" texto="Home" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
         <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
       </ul>
+      <ul>
+        <MenuItem
+          texto="Sair"
+          icone={IconeSair}
+          onClick={() => console.log("logout")}
+          className={`
+          text-red-600 dark:text-red-400
+         hover:bg-red-400 hover:text-white
+         dark:hover:text-white
+         `}
+        />
+      </ul>
     </aside>
   );
 }
-
