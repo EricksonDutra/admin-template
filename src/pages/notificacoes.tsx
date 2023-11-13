2f49d54faa19a6f6c2f031b0b387049c678a848f
EricksonDutra <ericksond10@gmail.com>
2023-11-13 19:07:28 -0400

    :construction: botões de tema

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,9 +1,11 @@
 import Layout from "@/components/template/Layout";
+import useAppData from "@/data/hook/useAppData";
 
 export default function Notificacoes() {
+  const {alternarTema } = useAppData()
   return (
-    <Layout titulo="Notificações" subtitulo="Suas notificações" >
-      <h3>Conteúdo</h3>
-      </Layout>
-  )
+    <Layout titulo="Notificações" subtitulo="Suas notificações">
+      <button onClick={alternarTema}> Click</button>
+    </Layout>
+  );
 }
